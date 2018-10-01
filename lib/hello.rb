81714942a9b4111f23c3030c375c22a71408a22c
Bobbi York <york1670@gmail.com>
2018-10-01 02:37:42 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,10 @@
-def hello_t(names)
-  names.each do |name|
-    yield(name)
+def hello_t(names)  #block  { |aName| puts aName }
+  if block_given?
+    names.each do |name|
+      yield(name)   #block(name)
+    end
+  else
+    puts "error message"
   end
   return names
 end
